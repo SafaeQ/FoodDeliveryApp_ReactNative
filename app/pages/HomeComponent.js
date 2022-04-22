f7e69dacfbdbaee03034efd2ab3b74da3a424ece
SafaeQ <safaedani009@gmail.com>
2022-04-22 11:57:40 +0000

    😊✔ initial setup

---
+++ app/pages/HomeComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeComponent = () => {
+  return (
+    <div>
+      <View>
+          <Text> hello dear</Text>
+      </View>
+    </div>
+  );
+}
+
+export default HomeComponent;
