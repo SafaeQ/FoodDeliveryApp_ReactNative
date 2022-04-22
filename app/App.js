280ae4332b0bf7cff3d5fb79730c5304c1670e51
SafaeQ <safaedani009@gmail.com>
2022-04-22 12:18:07 +0000

    😊✔ initial setup

--- app/App.js
+++ app/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import  HomeComponent  from "./pages/HomeComponent";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <HomeComponent/>
       <StatusBar style="auto" />
     </View>
   );
--- app/pages/HomeComponent.js
+++ app/pages/HomeComponent.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+const image = { uri: 'https://simply-delicious-food.com/wp-content/uploads/2016/08/Hash-brown-cheeseburgers-2.jpg' };
 
 const HomeComponent = () => {
   return (
     <div>
-      <View>
+      <View style={styles.container}>
+          <ImageBackground source={image} resizeMode="cover" style={styles.image}>
           <Text> hello dear</Text>
+          </ImageBackground>
       </View>
     </div>
   );
 }
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'pink',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+})
 export default HomeComponent;
