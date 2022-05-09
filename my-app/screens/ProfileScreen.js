5a8fe7f53573a31e1b1d0f161a0eadcc66489f87
SafaeQ <safaedani009@gmail.com>
2022-05-09 15:39:17 +0100

    🐛 add profile screen

--- my-app/screens/ProfileScreen.js
+++ my-app/screens/ProfileScreen.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableWithoutFeedback } from 'react-native';
+
+// Custom styles
+import { basic, form} from "../constants/style";
+import colors from "../constants/colors";
 
 
 const ProfileScreen = () => {
   return (
     <>
-      <View>
-          <Text> heyyy </Text>
+    <TouchableWithoutFeedback>
+        <View>
+        <Text style={[form.heading, form.field]}> Profile </Text>
       </View>
+    </TouchableWithoutFeedback>
     </>
   );
 }
