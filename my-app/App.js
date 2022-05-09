05318b5b7e1ba013e993dfa111bcf2909038ac39
SafaeQ <safaedani009@gmail.com>
2022-05-09 15:49:31 +0100

    🐛 add @react-navigation/bottom-tabs

--- my-app/App.js
+++ my-app/App.js
@@ -22,7 +22,7 @@ export default function App() {
       </NavigationContainer>
       {/* <HomeScreen/> */}
       {/* <SignupScreen/> */}
-      {/* <LoginScreen/> */}
+      {/* <ProfileScreen/> */}
     </>
     
   );
