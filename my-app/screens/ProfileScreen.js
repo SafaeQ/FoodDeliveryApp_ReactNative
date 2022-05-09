0eb1931727b0bebfc0e17f3e9422629504f65072
SafaeQ <safaedani009@gmail.com>
2022-05-09 15:11:12 +0100

    🐛 refactor my code..

---
+++ my-app/screens/ProfileScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+
+
+const ProfileScreen = () => {
+  return (
+    <>
+      <View>
+          <Text> heyyy </Text>
+      </View>
+    </>
+  );
+}
+
+export default ProfileScreen;
