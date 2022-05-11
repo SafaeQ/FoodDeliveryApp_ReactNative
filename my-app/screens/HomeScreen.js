ff5283f3eb234560561fb6b34c442dfe28196de3
SafaeQ <safaedani009@gmail.com>
2022-05-11 09:32:14 +0100

    ⚡️ refactor my code

--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -9,13 +9,32 @@ import colors from "../constants/colors";
 const HomeScreen = () => {
   return (
     <>
+    <ImageBackground
+        source={require('../assets/images/HAMBÚRGUER.jpg')}
+         resizeMode="contain"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+      </ImageBackground >
+
     <TouchableWithoutFeedback>
+
         <View>
         <Text style={[form.heading, form.field]}> Home </Text>
       </View>
+
     </TouchableWithoutFeedback>
     </>
   );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 1,
+  },
+})
