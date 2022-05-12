8dc1edfce4a8d8cf9e64dec125bb80ee8bc4caf6
SafaeQ <safaedani009@gmail.com>
2022-05-12 10:29:32 +0100

    ⚡️ refactor my code

--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -1,39 +1,58 @@
 
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions, Pressable, ImageBackground } from 'react-native';
 
 const data = [
   { key: 'A' }, { key: 'B' }, { key: 'C' }, { key: 'D' }, { key: 'E' }, { key: 'F' }, { key: 'G' }, { key: 'H' }, { key: 'I' }, { key: 'J' },{ key: 'J' }, { key: 'J' }
 ];
 
+// Custom styles
+import { basic, form} from "../constants/style";
+import colors from "../constants/colors";
 
 const numColumns = 2;
 
 export default function Home() {
+
   
- const renderItem = ({ item, index }) => {
-    if (item.empty === true) {
-      return <View style={[styles.item, styles.itemInvisible]} />;
-    }
+
+  
+ const renderItem = ({ item }) => {
+    
     return (
       <>
-      <View
-        style={styles.item}
+
+      <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
       >
-        <Text style={styles.itemText}>{item.key}</Text>
-      </View>
+        <View style={[styles.innerContainer, { backgroundColor: 'white' }]}>
+            <ImageBackground source={ require('../assets/images/téléchargement-removebg-preview (1).png')} style={{width: 151, height: 142}} >
+            </ImageBackground>
+              <Text style={styles.title}>{item.key}</Text>
+        </View>
+      </Pressable>
+    </View>
       </>
     );
   };
 
   
     return (
+      <>
+      <View>
+          <Text style={[form.heading, form.field]}> Home </Text>
+        </View>
       <FlatList
         data={data}
         style={styles.container}
         renderItem={renderItem}
         numColumns={numColumns}
       />
+      </>
     );
   
 }
@@ -56,6 +75,38 @@ const styles = StyleSheet.create({
   },
   itemText: {
     color: '#fff',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    // overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
