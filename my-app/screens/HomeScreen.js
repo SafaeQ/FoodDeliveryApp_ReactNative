a4b79396e5f407fe27b131622997b8bc88a962aa
SafaeQ <safaedani009@gmail.com>
2022-05-12 09:33:53 +0100

    ⚡️ refactor my code

--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -6,19 +6,9 @@ const data = [
   { key: 'A' }, { key: 'B' }, { key: 'C' }, { key: 'D' }, { key: 'E' }, { key: 'F' }, { key: 'G' }, { key: 'H' }, { key: 'I' }, { key: 'J' },{ key: 'J' }, { key: 'J' }
 ];
 
-// const formatData = (data, numColumns) => {
-//   const numberOfFullRows = Math.floor(data.length / numColumns);
-
-//   let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
-//   while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
-//     data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
-//     numberOfElementsLastRow++;
-//   }
-
-//   return data;
-// };
 
 const numColumns = 2;
+
 export default function Home() {
   
  const renderItem = ({ item, index }) => {
@@ -26,11 +16,13 @@ export default function Home() {
       return <View style={[styles.item, styles.itemInvisible]} />;
     }
     return (
+      <>
       <View
         style={styles.item}
       >
         <Text style={styles.itemText}>{item.key}</Text>
       </View>
+      </>
     );
   };
 
