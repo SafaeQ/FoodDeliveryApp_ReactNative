a7f2b29b08615ccebf0924bdf2b86969da5c1f01
SafaeQ <safaedani009@gmail.com>
2022-05-17 16:18:44 +0100

    ⚡️ refactor my code

--- my-app/screens/DetailScreen.js
+++ my-app/screens/DetailScreen.js
@@ -1,19 +1,27 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native-web';
+import { Image, StyleSheet, Text, View } from 'react-native-web';
 
 
 
 function Detail() {
     return ( 
         <>
-            <ImageBackground source= { require('../assets/images/images.jpg') } 
+        <View>
+            <View>
+            <Image source= { require('../assets/images/images.jpg') } 
             
-            resizeMode="contain" style={styles.rootScreen}>
-                
-            </ImageBackground>
+            resizeMode="contain" style={styles.rootScreen}/>
+        
+            </View>
 
             <View style={styles.textContainer}>
-                <Text style={styles.text} > Food </Text>
+                <Text style={styles.text}> Food </Text>
             </View>
+
+            <View>
+                <Text style={styles.paragraph}> Create React App doesn’t handle backend logic or databases; it just creates a frontend build pipeline, so you can use it with any backend you want. </Text>
+            </View>
+        </View>
+
         </>
      );
 }
@@ -37,6 +45,12 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: 'bold', 
         textAlign: 'center'
+    },
+    paragraph: {
+        textAlign: 'center',
+        // fontWeight: 'bold',
+        fontStyle: 'italic',
+        fontSize: 20,
     },
     buttonInnerContainer: {
       top: -49,
