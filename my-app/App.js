c63b9ccd8ede30a850e53b6f62109b43349d9d7a
SafaeQ <safaedani009@gmail.com>
2022-05-26 14:09:51 +0100

    ⚡️ refactor my code

--- my-app/App.js
+++ my-app/App.js
@@ -27,7 +27,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           {/* <Stack.Screen name="Detail" component={DetailScreen} /> */}
-          {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        
           <Stack.Screen name="Bord" component={BordScreen} />
           <Stack.Screen name="Login" component={LoginScreen } />
           <Stack.Screen name="Signup" component={SignupScreen} />
